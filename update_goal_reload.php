7f59fe240f378f5e3b5116e8c0b017dd3ec402c3
akima9 <akima9a@gmail.com>
2020-03-13 10:20:22 +0900

    목표개수 업데이트 작업

--- update_goal_reload.php
+++ update_goal_reload.php
@@ -32,9 +32,10 @@
         }
 
         // pre_goal_cnt 업데이트
-        $stmt = $con->prepare('UPDATE goal SET pre_goal_cnt = :pre_goal_cnt WHERE userId = :userId');
+        $stmt = $con->prepare('UPDATE goal SET pre_goal_cnt = :pre_goal_cnt, goal_cnt = :goal_cnt WHERE userId = :userId');
         $stmt->bindParam(':userId', $userId);
         $stmt->bindParam(':pre_goal_cnt', $today_goal_cnt[$i]);
+        $stmt->bindParam(':goal_cnt', 0);
         $stmt->execute();
     }
     
