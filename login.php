1536888d77f8bf2ed75b7e3f71357e3a706d41a0
akima9 <akima9a@gmail.com>
2020-03-13 10:29:21 +0900

    DB정보 github에서 빼기

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     error_reporting(E_ALL); 
     ini_set('display_errors',1); 
 
-    include('dbcon.php');
+    include('../dbcon.php');
 
 
     $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
