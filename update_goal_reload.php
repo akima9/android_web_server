c0f6e6e620331e56e283da38e0589cf62b4e8edc
akima9 <akima9a@gmail.com>
2020-03-31 10:19:29 +0900

    cron 작업 파일 수정 01

--- update_goal_reload.php
+++ update_goal_reload.php
@@ -23,37 +23,39 @@
         
         $userId = $result[$i][0];
         
-        // 각각의 userId에 대응하는 goal_cnt 받아오기
-        // $query = "SELECT goal_cnt FROM goal WHERE userid = ?";
-        $query = "SELECT todayCnt FROM person WHERE userid = ?";
+        // 각각의 userId에 대응하는 todayCnt, goal 받아오기
+        // todayCnt : 오늘 핀 개수
+        // goal : 목표 개수
+        $query = "SELECT todayCnt, goal FROM person WHERE userid = ?";
         $stmt = $con->prepare($query);
         $stmt->execute(array($userId));
         while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-            if($row[0] > 0) {
-                $today_goal_cnt[$cnt] = $row[0] - 1;
-                $total_goal_cnt[$cnt] = $row[0];
-            } else {
-                $today_goal_cnt[$cnt] = $row[0];
-                $total_goal_cnt[$cnt] = $row[0];
-            }
-            $cnt++;
+            var_dump($row);
+            // if($row[0] > 0) {
+            //     $today_goal_cnt[$cnt] = $row[0] - 1;
+            //     $total_goal_cnt[$cnt] = $row[0];
+            // } else {
+            //     $today_goal_cnt[$cnt] = $row[0];
+            //     $total_goal_cnt[$cnt] = $row[0];
+            // }
+            // $cnt++;
         }
 
         // goal 업데이트
-        $stmt = $con->prepare('UPDATE person SET goal = :goal, todayCnt = :todayCnt WHERE userId = :userId');
-        $stmt->bindParam(':userId', $userId);
-        $stmt->bindParam(':goal', $today_goal_cnt[$i]);
-        $stmt->bindParam(':todayCnt', $goal_cnt);
-        $stmt->execute();
+        // $stmt = $con->prepare('UPDATE person SET goal = :goal, todayCnt = :todayCnt WHERE userId = :userId');
+        // $stmt->bindParam(':userId', $userId);
+        // $stmt->bindParam(':goal', $today_goal_cnt[$i]);
+        // $stmt->bindParam(':todayCnt', $goal_cnt);
+        // $stmt->execute();
 
-        $crdate = date("Y-m-d H:i:s");
+        // $crdate = date("Y-m-d H:i:s");
 
-        $stmt = $con->prepare('INSERT INTO history(userId, todayCnt, goal, crdate) VALUES(:userId, :todayCnt, :goal, :crdate)');
-        $stmt->bindParam(':userId', $userId);
-        $stmt->bindParam(':todayCnt', $goal_cnt);
-        $stmt->bindParam(':goal', $total_goal_cnt[$i]);
-        $stmt->bindParam(':crdate', $crdate);
-        $stmt->execute();
+        // $stmt = $con->prepare('INSERT INTO history(userId, todayCnt, goal, crdate) VALUES(:userId, :todayCnt, :goal, :crdate)');
+        // $stmt->bindParam(':userId', $userId);
+        // $stmt->bindParam(':todayCnt', $goal_cnt);
+        // $stmt->bindParam(':goal', $total_goal_cnt[$i]);
+        // $stmt->bindParam(':crdate', $crdate);
+        // $stmt->execute();
     }
     
 
