483f679957cb04fb378c4c79c10dceb0dfab44d1
akima9 <akima9a@gmail.com>
2020-04-02 14:24:04 +0900

    select datetime

--- history_select.php
+++ history_select.php
@@ -26,7 +26,8 @@
         $crdate = $year."-".$month."-".$dayOfMonth;
 
         try{
-            $query = "SELECT goal, todayCnt FROM history WHERE userId = :userId, crdate LIKE :crdate";
+            $query = "SELECT goal, todayCnt FROM history WHERE userId = :userId, DATE(crdate) LIKE :crdate";
+            // DATE(post_date)='2012-01-22'
             $stmt = $con->prepare($query);
             // $stmt = bindParam(1, $userId, PDO::PARAM_STR);
             $stmt -> bindValue(":userId", $userId, PDO::PARAM_STR);
