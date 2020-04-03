b1e16539ea42a7f26703fcdd07c0ca88be63ed97
akima9 <akima9a@gmail.com>
2020-04-03 10:57:44 +0900

    history select 작업

--- history_select.php
+++ history_select.php
@@ -18,39 +18,50 @@
         $month=$_POST['month'];
         $dayOfMonth=$_POST['dayOfMonth'];
 
-        var_dump($userId);
-        var_dump($year);
-        var_dump($month);
-        var_dump($dayOfMonth);
+        // var_dump($userId);
+        // var_dump($year);
+        // var_dump($month);
+        // var_dump($dayOfMonth);
 
         $crdate = $year."-".$month."-".$dayOfMonth;
 
         try{
             $query = "SELECT goal, todayCnt FROM history WHERE userId = :userId and DATE(crdate) = :crdate";
-            // DATE(post_date)='2012-01-22'
             $stmt = $con->prepare($query);
-            // $stmt = bindParam(1, $userId, PDO::PARAM_STR);
             $stmt -> bindValue(":userId", $userId, PDO::PARAM_STR);
             $stmt -> bindValue(":crdate", $crdate, PDO::PARAM_STR);
-            // $stmt->execute(array($userId,$crdate));
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_NUM);
 
-            var_dump($result);
+            // var_dump($result);
 
-            // $response = array();
+            // array(1) {
+            //     [0]=>
+            //     array(2) {
+            //       [0]=>
+            //       string(2) "10"
+            //       [1]=>
+            //       string(1) "0"
+            //     }
+            //   }
+            // $result[0][0] // goal
+            // $result[0][1] // todayCnt
 
-            // if(isset($result[0][0])) {
-            //     $response["goalCnt"] = $result[0][0];
-            //     //$response["userId"] = $userId;
+            $response = array();
+
+            if(isset($result[0][0])) {
+                $response["goal"] = $result[0][0];
+                $response["todayCnt"] = $result[0][1];
+                $response["status"] = 200;
+                //$response["userId"] = $userId;
                 
-            //     // session_start();
-            //     // $_SESSION['userId'] = $userId;
-            // } else {
-            //     $response["goalCnt"] = 999;
-            // }
+                // session_start();
+                // $_SESSION['userId'] = $userId;
+            } else {
+                $response["status"] = 999;
+            }
 
-            // echo json_encode($response);
+            echo json_encode($response);
 
         } catch(PDOException $e) {
             die("Database error: " . $e->getMessage()); 
